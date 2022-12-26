070d4f588e00903db923ba81721e5b87975291a1
K <65287118+katzerax@users.noreply.github.com>
2022-12-25 22:31:12 -0800

    add ability to delay starting fire or not

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 
 # Predefined variables used in the functions, modifiable
-data_points = 4500  # please make data_points and xScale in multiples of 10
+data_points = 450000  # please make data_points and xScale in multiples of 10
 x_scale = 45  # scale of the X axis
 y_scale = 300000  # scale of the Y axis
 x_increments = x_scale / data_points
 
 # graph limits
 plt.xlim(0, x_scale)
-plt.ylim(0, 150000)
+plt.ylim(0, 300000)
 
 # establish x array
 x = []
@@ -18,13 +18,12 @@ for i in range(data_points):
 # Initialize list to store legend labels
 legend_labels = []
 
-def plot_dps_graph(fire_delay, reload_time, damage_per_shot, mag_cap, ammo_reserve, legend_label):
+def plot_dps_graph(fire_delay, reload_time, damage_per_shot, mag_cap, ammo_reserve, legend_label, delay_first_shot):
   # Initialize t_dmg list
   t_dmg = []
-
   shots_fired = 0
   next_fire = fire_delay
-  total_damage = 0
+  total_damage = 0 if delay_first_shot else damage_per_shot #* -1
   time_elapsed = 0
   shots_fired_total = 0
 
@@ -32,7 +31,7 @@ def plot_dps_graph(fire_delay, reload_time, damage_per_shot, mag_cap, ammo_reser
       if shots_fired_total == ammo_reserve:
           total_damage = total_damage
       else:
-          if shots_fired == mag_cap:
+          if shots_fired > mag_cap:
               next_fire += reload_time
               shots_fired = 0
           elif time_elapsed == next_fire:
@@ -59,10 +58,10 @@ def plot_dps_graph(fire_delay, reload_time, damage_per_shot, mag_cap, ammo_reser
   # Add legend label to list
   legend_labels.append(legend_label)
 
-# example function calls
-plot_dps_graph(0.7, 0.5, 56000, 7, 20, "dps 1")
-plot_dps_graph(0.6, 0.4, 55000, 8, 25, "dps 2")
-plot_dps_graph(0.5, 0.3, 54000, 9, 30, "dps 3")
+# example function calls (fire_delay, reload_time, damage_per_shot, mag_cap, ammo_reserve, legend_label, delay_first_shot)
+plot_dps_graph(0.067, 0.73, 5200, 150, 547, "mystery weapon 1", True)
+plot_dps_graph(3, 1.25, 193000, 2, 8, "rocket launcher", False)
+plot_dps_graph(0.5, 0.83, 98000, 8, 26, "charge up rifle", True)
 
 # Add a legend with all labels
 plt.legend(legend_labels)
