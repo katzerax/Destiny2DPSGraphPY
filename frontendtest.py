bc98923f498d6f637382ef58786989cc2cb24f52
K <65287118+katzerax@users.noreply.github.com>
2023-04-17 12:34:07 -0700

    fix checkboxes

--- frontendtest.py
+++ frontendtest.py
@@ -132,7 +132,7 @@ canvas.draw()
 canvas.get_tk_widget().grid(row=0, column=0, sticky='nsew')
 
 # Create the UI elements
-frame = ttk.Frame(app)
+frame = tk.Frame(app)
 frame.grid(row=0, column=1, padx=10, pady=10, sticky='n') # Sticks itself to the north section
 
 # Configure the frame to be resizable
@@ -142,57 +142,57 @@ frame.grid_rowconfigure(0, weight=1)
 
 # Add input fields and labels
 row_num = 0
-weapon_name_label = ttk.Label(frame, text="Weapon Name")
+weapon_name_label = tk.Label(frame, text="Weapon Name")
 weapon_name_label.grid(row=row_num, column=0, sticky='ew')
-weapon_name_entry = ttk.Entry(frame)
+weapon_name_entry = tk.Entry(frame)
 weapon_name_entry.grid(row=row_num, column=1, sticky='ew')
 row_num += 1
 
 row_num += 1
-fire_rate_label = ttk.Label(frame, text="Fire Rate")
+fire_rate_label = tk.Label(frame, text="Fire Rate")
 fire_rate_label.grid(row=row_num, column=0)
-fire_rate_entry = ttk.Entry(frame)
+fire_rate_entry = tk.Entry(frame)
 fire_rate_entry.grid(row=row_num, column=1)
 
 row_num += 1
-reload_time_label = ttk.Label(frame, text="Reload Time")
+reload_time_label = tk.Label(frame, text="Reload Time")
 reload_time_label.grid(row=row_num, column=0)
-reload_time_entry = ttk.Entry(frame)
+reload_time_entry = tk.Entry(frame)
 reload_time_entry.grid(row=row_num, column=1)
 
 row_num += 1
-damage_per_shot_label = ttk.Label(frame, text="Damage per Shot")
+damage_per_shot_label = tk.Label(frame, text="Damage per Shot")
 damage_per_shot_label.grid(row=row_num, column=0)
-damage_per_shot_entry = ttk.Entry(frame)
+damage_per_shot_entry = tk.Entry(frame)
 damage_per_shot_entry.grid(row=row_num, column=1)
 
 row_num += 1
-magazine_capacity_label = ttk.Label(frame, text="Magazine Capacity")
+magazine_capacity_label = tk.Label(frame, text="Magazine Capacity")
 magazine_capacity_label.grid(row=row_num, column=0)
-magazine_capacity_entry = ttk.Entry(frame)
+magazine_capacity_entry = tk.Entry(frame)
 magazine_capacity_entry.grid(row=row_num, column=1)
 
 row_num += 1
-ammo_reserve_label = ttk.Label(frame, text="Ammo Reserve")
+ammo_reserve_label = tk.Label(frame, text="Ammo Reserve")
 ammo_reserve_label.grid(row=row_num, column=0)
-ammo_reserve_entry = ttk.Entry(frame)
+ammo_reserve_entry = tk.Entry(frame)
 ammo_reserve_entry.grid(row=row_num, column=1)
 
 row_num += 1
 delay_first_shot_var = tk.BooleanVar()
-delay_first_shot_checkbox = ttk.Checkbutton(frame, text="Delay First Shot", variable=delay_first_shot_var)
+delay_first_shot_checkbox = tk.Checkbutton(frame, text="Delay First Shot", variable=delay_first_shot_var)
 delay_first_shot_checkbox.grid(row=row_num, columnspan=2)
 
 row_num += 1
-perks_frame = ttk.LabelFrame(frame, text="Perks")
+perks_frame = tk.LabelFrame(frame, text="Perks")
 perks_frame.grid(row=row_num, columnspan=2)
 
 perk1_var = tk.BooleanVar()
-perk1_checkbox = ttk.Checkbutton(perks_frame, text="Perk 1", variable=perk1_var)
+perk1_checkbox = tk.Checkbutton(perks_frame, text="Perk 1", variable=perk1_var)
 perk1_checkbox.grid(row=0, column=0)
 
 perk2_var = tk.BooleanVar()
-perk2_checkbox = ttk.Checkbutton(perks_frame, text="Perk 2", variable=perk2_var)
+perk2_checkbox = tk.Checkbutton(perks_frame, text="Perk 2", variable=perk2_var)
 perk2_checkbox.grid(row=0, column=1)
 
 #Buff Order as Follows:
@@ -200,50 +200,50 @@ perk2_checkbox.grid(row=0, column=1)
 #2 - 
 
 row_num += 1
-buffs_frame = ttk.LabelFrame(frame, text="Buffs")
+buffs_frame = tk.LabelFrame(frame, text="Buffs")
 buffs_frame.grid(row=row_num, columnspan=2)
 
 buff1_var = tk.BooleanVar()
-buff1_checkbox = ttk.Checkbutton(buffs_frame, text="Buff 1", variable=buff1_var)
+buff1_checkbox = tk.Checkbutton(buffs_frame, text="Buff 1", variable=buff1_var)
 buff1_checkbox.grid(row=0, column=0)
 
 buff2_var = tk.BooleanVar()
-buff2_checkbox = ttk.Checkbutton(buffs_frame, text="Buff 2", variable=buff2_var)
+buff2_checkbox = tk.Checkbutton(buffs_frame, text="Buff 2", variable=buff2_var)
 buff2_checkbox.grid(row=0, column=1)
 
 buff3_var = tk.BooleanVar()
-buff3_checkbox = ttk.Checkbutton(buffs_frame, text="Buff 3", variable=buff3_var)
+buff3_checkbox = tk.Checkbutton(buffs_frame, text="Buff 3", variable=buff3_var)
 buff3_checkbox.grid(row=0, column=2)
 
 # ... add more buff checkboxes as needed
 
 
 row_num += 1
-add_weapon_btn = ttk.Button(frame, text="Add Weapon", command=add_weapon)
+add_weapon_btn = tk.Button(frame, text="Add Weapon", command=add_weapon)
 add_weapon_btn.grid(row=row_num, columnspan=2)
 
 row_num += 1
-plot_graph_btn = ttk.Button(frame, text="Calculate & Plot Graph", command=calculate_and_plot)
+plot_graph_btn = tk.Button(frame, text="Calculate & Plot Graph", command=calculate_and_plot)
 plot_graph_btn.grid(row=row_num, columnspan=2)
 
 row_num += 1
-import_weapon_btn = ttk.Button(frame, text="Import Weapon", command=import_weapon)
+import_weapon_btn = tk.Button(frame, text="Import Weapon", command=import_weapon)
 import_weapon_btn.grid(row=row_num, columnspan=2)
 
 row_num += 1
-export_weapon_btn = ttk.Button(frame, text="Export Weapon", command=export_weapon)
+export_weapon_btn = tk.Button(frame, text="Export Weapon", command=export_weapon)
 export_weapon_btn.grid(row=row_num, columnspan=2)
 
 row_num += 1
-import_csv_btn = ttk.Button(frame, text="Import CSV", command=import_csv)
+import_csv_btn = tk.Button(frame, text="Import CSV", command=import_csv)
 import_csv_btn.grid(row=row_num, columnspan=2)
 
 row_num += 1
-export_csv_btn = ttk.Button(frame, text="Export CSV", command=export_csv)
+export_csv_btn = tk.Button(frame, text="Export CSV", command=export_csv)
 export_csv_btn.grid(row=row_num, columnspan=2)
 
 row_num += 1
-export_csv_btn = ttk.Button(frame, text="debug", command=debug_print)
+export_csv_btn = tk.Button(frame, text="debug", command=debug_print)
 export_csv_btn.grid(row=row_num, columnspan=2)
 
 
