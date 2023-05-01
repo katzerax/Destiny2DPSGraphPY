08f30b92bad85ea6362680119e6aab4858261e20
K <65287118+katzerax@users.noreply.github.com>
2023-04-30 23:25:30 -0700

    uhhhm a lot
    yknow,,

--- frontendrewrite.py
+++ frontendrewrite.py
@@ -4,6 +4,8 @@ import os
 import configparser
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from weaponclassrewrite import Weapon
+
 
 class Settings:
     def __init__(self):
@@ -54,9 +56,11 @@ class GUI(tk.Frame):
             self.master.configure(bg='#1E1E1E')
             self.check_button_style = {'bg': '#1E1E1E', 'fg': '#CCCCCC', 'selectcolor': '#1E1E1E'}
             self.button_style = {'bg': '#1E1E1E', 'fg': '#CCCCCC'}
+            self.matplotlibbg = "#1E1E1E"
         else:
             self.check_button_style = {}
             self.button_style = {}
+            self.matplotlibbg = "#FFFFFF"
 
         if self.settings.cmd_prints == 'True':
             pass
@@ -78,46 +82,133 @@ class GUI(tk.Frame):
         self.fig = Figure(figsize=(5, 4), dpi=100)
         self.ax = self.fig.add_subplot(111)
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)
-        self.plot_data()
+        self.ax.set_facecolor(self.matplotlibbg)
+        self.fig.set_facecolor(self.matplotlibbg)
 
-        # Create a tkinter check button
-        self.var = tk.BooleanVar()
-        self.check_button = tk.Checkbutton(self, text="Check Box", variable=self.var, command=self.update_graph, **self.check_button_style)
+        # Create a Frame to hold input boxes, labels, and checkboxes
+        self.inputs_frame = tk.Frame(self, bg=self.button_style.get('bg', ''))
+        self.inputs_frame.pack(side=tk.RIGHT, fill=tk.Y, padx=5, pady=5)
 
-        # Create a tkinter button to randomize the graph
-        self.randomize_button = tk.Button(self, text="Randomize Graph", command=self.randomize_graph, **self.button_style)
+        #name, fire_rate, reload_time, damage_per_shot, mag_cap, ammo_total, delay_first_shot, burst_weapon, burst_bullets, swap_group, swap_time
+
+        # Create input boxes with labels
+        self.name_label = tk.Label(self.inputs_frame, text="Name", **self.button_style)
+        self.name_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+        self.name_entry = tk.Entry(self.inputs_frame)
+        self.name_entry.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
+
+        self.fire_rate_label = tk.Label(self.inputs_frame, text="Fire Rate", **self.button_style)
+        self.fire_rate_label.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)  # Change row number to 1
+        self.fire_rate_entry = tk.Entry(self.inputs_frame)
+        self.fire_rate_entry.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)  # Change row number to 1
+
+        self.reload_time_label = tk.Label(self.inputs_frame, text="Reload Time", **self.button_style)
+        self.reload_time_label.grid(row=2, column=0, padx=5, pady=5, sticky=tk.W)
+        self.reload_time_entry = tk.Entry(self.inputs_frame)
+        self.reload_time_entry.grid(row=2, column=1, padx=5, pady=5, sticky=tk.W)
+
+        self.damage_per_shot_label = tk.Label(self.inputs_frame, text="Damage per Shot", **self.button_style)
+        self.damage_per_shot_label.grid(row=3, column=0, padx=5, pady=5, sticky=tk.W)
+        self.damage_per_shot_entry = tk.Entry(self.inputs_frame)
+        self.damage_per_shot_entry.grid(row=3, column=1, padx=5, pady=5, sticky=tk.W)
+
+        self.mag_cap_label = tk.Label(self.inputs_frame, text="Magazine Capacity", **self.button_style)
+        self.mag_cap_label.grid(row=4, column=0, padx=5, pady=5, sticky=tk.W)
+        self.mag_cap_entry = tk.Entry(self.inputs_frame)
+        self.mag_cap_entry.grid(row=4, column=1, padx=5, pady=5, sticky=tk.W)
+
+        self.ammo_total_label = tk.Label(self.inputs_frame, text="Ammo Total", **self.button_style)
+        self.ammo_total_label.grid(row=5, column=0, padx=5, pady=5, sticky=tk.W)
+        self.ammo_total_entry = tk.Entry(self.inputs_frame)
+        self.ammo_total_entry.grid(row=5, column=1, padx=5, pady=5, sticky=tk.W)
 
 
-        # Configure row and column weights for expansion
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=0)
+        # Create checkboxes
+        self.delay_first_shot_var = tk.BooleanVar()
+        self.delay_first_shot_button = tk.Checkbutton(self.inputs_frame, text="Delay First Shot", variable=self.delay_first_shot_var, command=self.update_graph, **self.check_button_style)
+        self.delay_first_shot_button.grid(row=6, column=0, padx=5, pady=5, sticky=tk.W)
 
-        # Add the Matplotlib canvas, check button, and randomize button to the GUI using the grid method
-        self.canvas.get_tk_widget().grid(row=0, column=0, rowspan=2, sticky=tk.NSEW)  # Use rowspan and sticky options
-        self.check_button.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
-        self.randomize_button.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)
+        self.burst_weapon_var = tk.BooleanVar()
+        self.burst_weapon_check_button = tk.Checkbutton(self.inputs_frame, text="Burst Weapon", variable=self.burst_weapon_var, command=self.update_graph, **self.check_button_style)
+        self.burst_weapon_check_button.grid(row=6, column=1, padx=5, pady=5, sticky=tk.W)
 
-    def plot_data(self):
+        # Create additional input boxes
+        self.burst_bullets_label = tk.Label(self.inputs_frame, text="Burst Bullets", **self.button_style)
+        self.burst_bullets_label.grid(row=7, column=0, padx=5, pady=5, sticky=tk.W)
+        self.burst_bullets_entry = tk.Entry(self.inputs_frame)
+        self.burst_bullets_entry.grid(row=7, column=1, padx=5, pady=5, sticky=tk.W)
+
+        self.swap_group_label = tk.Label(self.inputs_frame, text="Swap Group", **self.button_style)
+        self.swap_group_label.grid(row=8, column=0, padx=5, pady=5, sticky=tk.W)
+        self.swap_group_entry = tk.Entry(self.inputs_frame)
+        self.swap_group_entry.grid(row=8, column=1, padx=5, pady=5, sticky=tk.W)
+
+        self.swap_time_label = tk.Label(self.inputs_frame, text="Swap Time", **self.button_style)
+        self.swap_time_label.grid(row=9, column=0, padx=5, pady=5, sticky=tk.W)
+        self.swap_time_entry = tk.Entry(self.inputs_frame)
+        self.swap_time_entry.grid(row=9, column=1, padx=5, pady=5, sticky=tk.W)
+
+        # Create perk and buff checkboxes
+        self.triple_tap_var = tk.BooleanVar()
+        self.triple_tap_button = tk.Checkbutton(self.inputs_frame, text="Triple Tap", variable=self.triple_tap_var, command=self.update_graph, **self.check_button_style)
+        self.triple_tap_button.grid(row=10, column=0, padx=5, pady=5, sticky=tk.W)
+
+        self.FTTC_var = tk.BooleanVar()
+        self.FTTC_button = tk.Checkbutton(self.inputs_frame, text="FTTC", variable=self.FTTC_var, command=self.update_graph, **self.check_button_style)
+        self.FTTC_button.grid(row=10, column=1, padx=5, pady=5, sticky=tk.W)
+
+        # Create a button to create a Weapon object
+        self.create_weapon_button = tk.Button(self.inputs_frame, text="Create Weapon", command=self.create_weapon, **self.button_style)
+        self.create_weapon_button.grid(row=15, column=0, padx=5, pady=5, sticky=tk.W)
+
+        # Add the Matplotlib canvas to the GUI using the pack method
+        self.canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+
+
+    def create_weapon(self):
+        # Retrieve values from input boxes and checkboxes
+        name = self.name_entry.get()
+        fire_rate = float(self.fire_rate_entry.get())
+        reload_time = float(self.reload_time_entry.get())
+        damage_per_shot = float(self.damage_per_shot_entry.get())
+        mag_cap = int(self.mag_cap_entry.get())
+        ammo_total = int(self.ammo_total_entry.get())
+        delay_first_shot = self.delay_first_shot_var.get()
+        burst_weapon = self.burst_weapon_var.get()
+        burst_bullets = int(self.burst_bullets_entry.get()) if burst_weapon else 1
+        swap_group = int(self.swap_group_entry.get())
+        swap_time = float(self.swap_time_entry.get())
+
+        # Create the Weapon object using the retrieved values
+        weapon = Weapon(name, fire_rate, reload_time, damage_per_shot, mag_cap, ammo_total, delay_first_shot, burst_weapon, burst_bullets, swap_group, swap_time)
+
+        # Print the weapon details for debugging purposes
+        print("Created weapon: ", weapon.__dict__)
+
+
+
+    #def plot_data(self):
         # Generate some random data and plot it
-        x = [1, 2, 3, 4, 5]
-        y = [random.randint(1, 10) for i in range(5)]
-        self.ax.clear()
-        self.ax.plot(x, y)
-        self.canvas.draw()
+    #    x = [1, 2, 3, 4, 5]
+    #    y = [random.randint(1, 10) for i in range(5)]
+    #    self.ax.clear()
+    #    self.ax.plot(x, y)
+    #    self.canvas.draw()
 
     def update_graph(self):
-        # Print the state of the check box (True or False)
-        print(self.var.get())
+        print("burst:", self.burst_weapon_var.get())
+        print("delay:", self.delay_first_shot_var.get())
+        print("perk1:", self.triple_tap_var.get())
+        print("perk2:", self.FTTC_var.get())
 
-    def randomize_graph(self):
+    #def randomize_graph(self):
         # Randomize the graph data and redraw the canvas
-        x = [1, 2, 3, 4, 5]
-        y = [random.randint(1, 10) for i in range(5)]
-        self.ax.clear()
-        self.ax.plot(x, y)
-        self.canvas.draw()
+    #    x = [1, 2, 3, 4, 5]
+    #    y = [random.randint(1, 10) for i in range(5)]
+    #    self.ax.clear()
+    #    self.ax.plot(x, y)
+    #    self.canvas.draw()
 
 root = tk.Tk()
 app = GUI(master=root)
