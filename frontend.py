15f5890e1d730fcc1cce586dd360efc624b62ec3
Placni <pat.lacni@yahoo.com>
2023-05-05 21:27:15 -0500

    better restart, changed some naming conventions

--- frontend.py
+++ frontend.py
@@ -16,11 +16,8 @@ import weaponclassrewrite as backend
 
 # TODO
 # 1. Default ini
-# 2. Weapons menu // Talk to rox about multi-wep
-# 3. Log menu // normal vs verbose? (see log_menu comments)
-# 4. Make graph menu functional // 50% done
-# 5. Add graph config to settings or graph menu?
-# 6. Look into on-hover tooltips
+# 2. Make graph menu functional // 50% done
+# 3. Look into on-hover tooltips
 
 class Settings:
     def __init__(self):
@@ -30,15 +27,15 @@ class Settings:
         with open(ini_path, 'r', encoding='utf-8') as f:
             self.config.read_file(f)
         self.interface_theme = self.config.get('Interface', 'theme')
-        self.cmd_prints = self.config.getboolean('Interface', 'cmdprints')
+        self.log_mode = self.config.get('Interface', 'log_mode')
         self.do_auto_save = self.config.getboolean('AutoSave', 'enabled')
         self.auto_save_path = self.config.get('AutoSave', 'path')
 
     def set_interface_theme(self, theme):
         self.interface_theme = theme
 
-    def set_cmd_prints(self, value):
-        self.cmd_prints = value
+    def set_log_mode(self, value):
+        self.log_mode = value
 
     def set_do_auto_save(self, value):
         self.do_auto_save = value
@@ -47,8 +44,8 @@ class Settings:
         self.auto_save_path = value
 
     def save_settings(self):
-        self.config.set('Interface', 'Theme', self.interface_theme)
-        self.config.set('Interface', 'CMDPrints', str(self.cmd_prints))
+        self.config.set('Interface', 'theme', self.interface_theme)
+        self.config.set('Interface', 'log_mode', self.log_mode)
         self.config.set('AutoSave', str(self.do_auto_save))
         self.config.set('AutoSave', self.auto_save_path)
 
@@ -58,15 +55,11 @@ class Settings:
     def reset_settings(self):
         pass
 
-    def restart_program(self, root):
+    def restart_gui(self, root):
         # Close the tkinter window
         root.destroy()
-
-        # Re-run the script
-            # NOTE execl as an executable is really volatile
-            # maybe we look into another way to reload app? -mys
-            # yeah prolly, I just wrote this quickly to have something that works but by all means do it better
-        os.execl(sys.executable, sys.executable, *sys.argv)
+        # Restart gui
+        global_start_gui()
 
 class GUI(tk.Frame):
     def __init__(self, master=None):
@@ -108,7 +101,7 @@ class GUI(tk.Frame):
             self.white_text = "#000000"
             self.matplotlib_fg = "#000000"
 
-        if self.settings.cmd_prints == 'True':
+        if self.settings.log_mode == 'True':
             pass
         else:
             pass
@@ -151,9 +144,9 @@ class GUI(tk.Frame):
         # Set default selection to 'Graph'
         self.nav_listbox.select_set(0)
         # Handle clicks on the navbar
-        self.nav_listbox.bind('<<ListboxSelect>>', self.toggle_menus)
+        self.nav_listbox.bind('<<ListboxSelect>>', self.navbar_toggle_menus)
 
-    def toggle_menus(self, evt):
+    def navbar_toggle_menus(self, evt):
         # Grab which option was clicked
         index = int(evt.widget.curselection()[0])
         match index:
@@ -199,7 +192,7 @@ class GUI(tk.Frame):
         self.graph_wep_select_combo = ttk.Combobox(self.graph_wep_frame, values=wep_count, width=3, state='readonly')
         self.graph_wep_select_combo.grid(row=0, column=1, **self.default_padding)
         self.graph_wep_select_combo.set(wep_count[2])
-        self.graph_wep_select_combo.bind("<<ComboboxSelected>>", self.toggle_weapon_combos)
+        self.graph_wep_select_combo.bind("<<ComboboxSelected>>",lambda e: self.graph_frame.focus())
 
         # Build weapon select widgets
         self.graph_wep_widgets = [
@@ -219,11 +212,13 @@ class GUI(tk.Frame):
                 combo.grid_forget()
 
         # Generate graph button
-        self.graph_generate_button = tk.Button(self.graph_wep_frame, text="Generate Graph", command=self.generate_graph, **self.button_style)
+        self.graph_generate_button = tk.Button(self.graph_wep_frame, text="Generate Graph",
+                                                command=self.graph_generate_graph, **self.button_style)
         self.graph_generate_button.grid(row=15, column=0, padx=8, pady=5, sticky=tk.W)
 
         # Save graph button
-        self.graph_save_button = tk.Button(self.graph_wep_frame, text="Save Graph", command=self.save_graph, **self.button_style)
+        self.graph_save_button = tk.Button(self.graph_wep_frame, text="Save Graph", 
+                                           command=self.graph_save_graph, **self.button_style)
         self.graph_save_button.grid(row=15, column=1, padx=8, pady=5, sticky=tk.W)
 
         mpl.rcParams['text.color'] = self.matplotlib_fg
@@ -246,21 +241,19 @@ class GUI(tk.Frame):
         self.ax.set_xlim(0, 45)
         self.ax.set_ylim(0, 300000)
 
-        self.matplotlib_fg
-
         self.canvas.get_tk_widget().pack(side=tk.RIGHT, fill=tk.BOTH, expand=True, padx=15, pady=15)
 
-    def generate_graph(self):
+    def graph_generate_graph(self):
         pass
 
-    def save_graph(self):
+    def graph_save_graph(self):
         file_path = asksaveasfile(defaultextension='.png', filetypes=[('All Files', '*.*')], initialdir='./', initialfile='dps_graph.png')
         if file_path is None:
             return
         self.fig.savefig(file_path.name)
         print(f'Saved graph as "{file_path.name}"')
 
-    def toggle_weapon_combos(self, evt):
+    def graph_numweapons(self, evt):
         # Get ammount of weps requested
         ammount = int(self.graph_wep_select_combo.get()) - 1
 
@@ -282,8 +275,8 @@ class GUI(tk.Frame):
         self.weapons_frame.pack(side=tk.LEFT, fill=tk.Y)
 
         # Input validation
-        val_float = (self.register(self.validate_float))
-        val_int = (self.register(self.validate_int))
+        val_float = (self.register(self.weapons_val_float))
+        val_int = (self.register(self.weapons_val_int))
 
         # Combobox options
         type_options = ['Single Weapon', 'Weapon Swap']
@@ -318,13 +311,18 @@ class GUI(tk.Frame):
                       ttk.Combobox(workingframe, values=perk_choices, **self.combo_style)),
             'perk2': (tk.Label(workingframe, text="Perk 2", **self.label_style),
                       ttk.Combobox(workingframe, values=perk_choices, **self.combo_style)),
-            'enhance_toggle': (tk.Checkbutton(workingframe, text="Perk 1 Enhanced", variable=self.weapons_menu_vars['enhance1'], **self.check_button_style),
-                               tk.Checkbutton(workingframe, text="Perk 2 Enhanced", variable=self.weapons_menu_vars['enhance2'], **self.check_button_style)),
-            'burst_fusion_toggle': (tk.Checkbutton(workingframe, text="Burst Weapon", variable=self.weapons_menu_vars['burst_wep'], command=self.toggle_burst, **self.check_button_style),
-                                    tk.Checkbutton(workingframe, text="Fusion Weapon", variable=self.weapons_menu_vars['fusion_wep'], **self.check_button_style)),
+            'enhance_toggle': (tk.Checkbutton(workingframe, text="Perk 1 Enhanced", 
+                                              variable=self.weapons_menu_vars['enhance1'], **self.check_button_style),
+                               tk.Checkbutton(workingframe, text="Perk 2 Enhanced", 
+                                              variable=self.weapons_menu_vars['enhance2'], **self.check_button_style)),
+            'burst_fusion_toggle': (tk.Checkbutton(workingframe, text="Burst Weapon", 
+                                                   variable=self.weapons_menu_vars['burst_wep'],
+                                                    command=self.weapons_toggle_burst, **self.check_button_style),
+                                    tk.Checkbutton(workingframe, text="Fusion Weapon", 
+                                                   variable=self.weapons_menu_vars['fusion_wep'], **self.check_button_style)),
             'burst_bullets': (tk.Label(workingframe, text="Bullets Per Burst", **self.label_style),
                               tk.Entry(workingframe, validate='key', validatecommand=(val_int, '%S'))),
-            'create_wep': tk.Button(workingframe, text="Create Weapon", command=self.create_weapon_handler, **self.button_style)
+            'create_wep': tk.Button(workingframe, text="Create Weapon", command=self.weapons_create_weapon_handler, **self.button_style)
         }
 
         # Default combobox vals
@@ -363,7 +361,7 @@ class GUI(tk.Frame):
         # self.wep_type_label = tk.Label(self.weapons_frame, text="Type", **self.button_style)
         # self.wep_type_label.grid(row=0, column=0, **self.default_padding)
 
-    def toggle_burst(self):
+    def weapons_toggle_burst(self):
         label, entry, gridpos = self.weapons_menu_widgets['burst_bullets']
         if label.winfo_ismapped():
             label.grid_forget()
@@ -372,21 +370,21 @@ class GUI(tk.Frame):
             label.grid(row=gridpos, column=0, **self.default_padding)
             entry.grid(row=gridpos, column=1, **self.default_padding)
 
-    def validate_float(self, char):
+    def weapons_val_float(self, char):
         if char in '0123456789.':
             return True
         else:
             return False
         
-    def validate_int(self, char):
+    def weapons_val_int(self, char):
         if char in '0123456789':
             return True
         else:
             return False
 
-    def create_weapon_handler(self):
-        exitcode = self.create_weapon()
-        basestr = f'Weapon creation exited with error code {exitcode}:'
+    def weapons_create_weapon_handler(self):
+        exitcode = self.weapons_create_weapon()
+        basestr = f'Weapon creation exited with code {exitcode}:'
         match exitcode:
             case 0:
                 # NOTE Having a popup for a success is kinda aids ill work on something else
@@ -405,7 +403,7 @@ class GUI(tk.Frame):
                 messagebox.showerror('Creation Error', 'There was an error creating your weapon')
                 print(f'{basestr} Unknown Error')
         
-    def create_weapon(self):
+    def weapons_create_weapon(self):
         # Name validation
         name = self.weapons_menu_widgets['name'][1].get()
         if type(name) is str:
@@ -508,11 +506,14 @@ class GUI(tk.Frame):
             # Import / Export
             'impexp': {
                 'header': tk.Label(workingframe, text='Import / Export', **self.label_style),
-                'export': (tk.Button(workingframe, text='Export As', command=self.export_wep_list, **self.button_style),
+                'export': (tk.Button(workingframe, text='Export As', 
+                                     command=self.options_export_weps_handler, **self.button_style),
                         ttk.Combobox(workingframe, values=impexp_exp_exts, **self.combo_style)),
-                'log_impff': (tk.Button(workingframe, text='Log Current Weapons', command=self.print_wep_list, **self.button_style),
+                'log_impff': (tk.Button(workingframe, text='Log Current Weapons', 
+                                        command=self.options_print_weps, **self.button_style),
                             tk.Button(workingframe, text='Import From File', **self.button_style)),
-                'auto_save_toggle': tk.Checkbutton(workingframe, text='Auto Save / Load', command=self.toggle_auto_save, **self.check_button_style),
+                'auto_save_toggle': tk.Checkbutton(workingframe, text='Auto Save / Load', 
+                                                   command=self.options_toggle_autosave, **self.check_button_style),
                 'auto_save_path': (tk.Button(workingframe, text='Auto-Save Path', **self.button_style),
                                   tk.Entry(workingframe, state='disabled', **self.label_style))
             },
@@ -573,9 +574,9 @@ class GUI(tk.Frame):
         print(json.dumps(d, indent=4))
         pass
 
-    def export_wep_list_handler(self):
-        exitcode = self.export_wep_list()
-        basestr = f'Weapon export exited with error code {exitcode}:'
+    def options_export_weps_handler(self):
+        exitcode = self.options_export_weps()
+        basestr = f'Weapon Export exited with code {exitcode}:'
         match exitcode:
             case 0:
                 # NOTE Having a popup for a success is kinda aids ill work on something else
@@ -591,7 +592,7 @@ class GUI(tk.Frame):
                 print(f'An exception occured during Weapon Export:')
                 pprint(exitcode)
 
-    def export_wep_list(self):
+    def options_export_weps(self):
         if len(backend.weapons_list) < 0:
             return 1
         ext = self.options_menu_widgets['impexp']['export'][1].get()
@@ -624,7 +625,7 @@ class GUI(tk.Frame):
         except BaseException as e:
             return e
     
-    def toggle_auto_save(self):
+    def options_toggle_autosave(self):
         label, entry, gridpos = self.options_menu_widgets['impexp']['auto_save_path']
         if label.winfo_ismapped():
             label.grid_forget()
@@ -634,18 +635,18 @@ class GUI(tk.Frame):
             label.grid(**grid1, **self.default_padding)
             entry.grid(**grid2, **self.default_padding)
 
-    def print_wep_list(self):
+    def options_print_weps(self):
         print('Current list of weapons:\n')
         for weapon in backend.weapons_list.values():
             settings = weapon.get_pruned_settings()
             pprint(settings, sort_dicts=False)
             print()
 
-    def apply_settings(self):
+    def options_apply_settings(self):
         self.settings.set_interface_theme(self.setting1_combo.get())
-        self.settings.set_cmd_prints(self.setting2_combo.get())
+        self.settings.set_log_mode(self.setting2_combo.get())
         self.settings.save_settings()
-        self.settings.restart_program(root)
+        self.settings.restart_gui(root)
 
     def log_menu(self):
         self.log_frame = tk.Frame(self, **self.frame_style)
@@ -681,6 +682,10 @@ class TextRedirector:
     def flush(self):
         pass
 
-root = tk.Tk()
-app = GUI(master=root)
-app.mainloop()
+def global_start_gui():
+    global root
+    root = tk.Tk()
+    app = GUI(master=root)
+    app.mainloop()
+
+global_start_gui()
